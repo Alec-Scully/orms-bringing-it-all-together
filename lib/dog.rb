5ce40b62ba14913e84bb9ba8aaf670076601a883
Alec Scully <ScullyAlec@gmail.com>
2020-11-30 21:07:50 -0500

    commit tree 2a4e46245275457d1a93baa571599aeddd8d6a67

---
+++ lib/dog.rb
@@ -0,0 +1,39 @@
+class Dog
+    attr_accessor :name, :breed
+    attr_reader :id
+
+    def initialize(id = nil, name, breed)
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+    def self.create_table
+        sql = <<-SQL
+            CREATE TABLE dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
+        SQL
+        DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        sql = "DROP TABLE dogs"
+        DB[:conn].execute(sql)
+    end
+
+    def save
+        sql = <<-SQL
+            INSERT INTO dogs (name, breed)
+            VALUE(?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.breed)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+
+    def self.create(name:, breed:)
+        
+    end
+end
